bb785ce9d5510891db9b524ac1428d39664991cd
dimasyoga42 <dimasyoga9090@gmail.com>
2026-08-31 22:40:40 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Navbar from "./components/navbar.svelte"
+</script>
+
+<div class="flex max-w-6xl mx-auto">
+  <div>
+    <Navbar />
+  </div>
+</div>
---
+++ src/components/navbar.svelte
@@ -0,0 +1,10 @@
+
+<script>
+</script>
+
+<div class="w-full h-16 flex justify-items-center items-center">
+  <div class="flex justify-items-center items-center gap-1">
+    <img src="https://i.pinimg.com/736x/61/44/f0/6144f0ce1b33b5abe07cba04d2990d77.jpg" alt="pp" class="w-12 h-12 bg-cover rounded-full" />
+    <h1 class="font-extrabold ml-2">ShinraApi</h1>
+  </div>
+</div>
